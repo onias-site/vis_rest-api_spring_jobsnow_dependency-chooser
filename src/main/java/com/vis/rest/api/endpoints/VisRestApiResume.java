0ae358ef2766421f28a440ece94ace3133efb195
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2025-12-13 00:23:49 -0300

    ajustes de front end, finalmente está saindo

--- src/main/java/com/vis/rest/api/endpoints/VisRestApiResume.java
+++ src/main/java/com/vis/rest/api/endpoints/VisRestApiResume.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ccp.decorators.CcpJsonRepresentation;
@@ -21,10 +21,8 @@ import com.vis.services.VisServiceResume;
 @RequestMapping("/resume/{email}")
 public class VisRestApiResume{
 	
-	@RequestMapping(method = {RequestMethod.POST, RequestMethod.PATCH}, path = "/language/{language}")
-	public Map<String, Object> save(
-				@RequestBody Map<String, Object> sessionValues
-			) {
+	@PostMapping("/language/{language}")
+	public Map<String, Object> save(@RequestBody Map<String, Object> sessionValues) {
 
 		Map<String, Object> result = VisServiceResume.Save.execute(sessionValues);
 
@@ -39,7 +37,7 @@ public class VisRestApiResume{
 		return result;
 	}
 
-	@DeleteMapping("/status")
+	@DeleteMapping("/language/{language}/status")
 	public Map<String, Object> changeStatus(@RequestBody Map<String, Object> sessionValues){
 		
 		Map<String, Object> result = VisServiceResume.ChangeStatus.execute(sessionValues);
@@ -58,9 +56,7 @@ public class VisRestApiResume{
 	}
 
 	@GetMapping("/viewMode/{viewMode}")
-	public Map<String, Object> getFile(
-			@PathVariable("viewMode") String viewMode, 
-			@RequestBody Map<String, Object> sessionValues){
+	public Map<String, Object> getFile(@PathVariable("viewMode") String viewMode, @RequestBody Map<String, Object> sessionValues){
 		
 		CcpJsonRepresentation json = new CcpJsonRepresentation(sessionValues).put(JsonFieldNames.viewMode, viewMode);
 		
@@ -73,6 +69,7 @@ public class VisRestApiResume{
 	public String oi() {
 		return "oi";
 	}
+	
 	enum JsonFieldNames implements CcpJsonFieldName{
 		viewMode
 	}
