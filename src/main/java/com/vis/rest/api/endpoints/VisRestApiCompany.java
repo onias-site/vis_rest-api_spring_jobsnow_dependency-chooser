74c94dc9c89ed35105dcf791cd8f6240191213ee
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2026-06-18 02:20:53 -0300

    refatoraçoes

--- src/main/java/com/vis/rest/api/endpoints/VisRestApiCompany.java
+++ src/main/java/com/vis/rest/api/endpoints/VisRestApiCompany.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ccp.constantes.CcpOtherConstants;
+import com.ccp.constants.CcpOtherConstants;
 import com.vis.rest.open.api.VisOpenApiCompany;
 import com.vis.services.VisServiceCompany;
 
